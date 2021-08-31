96f55ca9061dcc50271fea2700a61d328a6dc0fc
code-ksu <kseniia.teslenko@gmail.com>
2021-08-31 12:54:52 +0200

    test run

--- start_scraping.bat
+++ start_scraping.bat
@@ -2,6 +2,6 @@ C:\ProgramData\Miniconda3\Scripts\activate.bat scraping
 
 cd opensea
 
-scrapy crawl os-collection -O art-blocks-2021-08-31.json
+scrapy crawl os-collection -O ..\data\art-blocks-2021-08-31.json
 
 cd ..
