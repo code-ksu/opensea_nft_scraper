1a417e16753b33c90aea1d0bc7c344f30ac4891a
code-ksu <kseniia.teslenko@gmail.com>
2021-11-01 16:29:48 +0100

    new fields

--- opensea/items.py
+++ opensea/items.py
@@ -7,22 +7,29 @@ from scrapy.item import Item, Field
 
 
 class NFT(Item):
-    # all from open sea
+    # meta
     os_id = Field()
     token_id = Field()
     name = Field()
     description = Field()
-    image_url = Field()
-    image_original_url = Field()
-    animation_original_url = Field()
     external_link = Field()
     permalink = Field()
     collection_slug = Field()
     creator = Field()
+    is_animation = Field()
+    # content
+    image_preview_url = Field()
+    image_url = Field()
+    image_original_url = Field()
+    animation_original_url = Field()
+    # price
     latest_sale_usd = Field()
     sell_order_usd = Field()
+    buy_order_usd = Field()
     # social
     instagram_follower = Field()
     twitter_follower = Field()
     medium_follower = Field()
+    telegram_follower = Field()
+    wiki_exists = Field()
 
